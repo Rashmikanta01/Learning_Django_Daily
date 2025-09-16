d7009152057d68f906e198eafde745da00b9bf11
Rashmi Kanta Biswal <167099176+Rashmikanta01@users.noreply.github.com>
2025-09-16 23:18:10 +0530

    dwnld_btstrp

---
+++ dwnld_btstrp/dwnld_btstrp/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for dwnld_btstrp project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dwnld_btstrp.settings')
+
+application = get_asgi_application()
