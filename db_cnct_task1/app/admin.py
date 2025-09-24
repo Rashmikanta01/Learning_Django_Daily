1f2be5d8a638c3e73f73f067e909a88cf3d41485
Rashmi Kanta Biswal <167099176+Rashmikanta01@users.noreply.github.com>
2025-09-24 21:38:10 +0530

    db_connection

---
+++ db_cnct_task1/app/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from app.models import Capitals, Countries
+
+# Register your models here.
+admin.site.register(Countries)
+admin.site.register(Capitals)
---
+++ db_cnct_task1/app/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+
+# Create your models here.
+class Countries(models.Model):
+    countries_id=models.IntegerField(primary_key=True)
+    Countries_name=models.CharField(max_length=100)
+class Capitals(models.Model):
+    Countries_id=models.OneToOneField(Countries,on_delete=models.CASCADE)
+    name=models.CharField(max_length=100)
+    url=models.URLField()
+
