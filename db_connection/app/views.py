0e03e282fcdc5bac156cbb493fea2add3d6fda2b
Rashmi Kanta Biswal <167099176+Rashmikanta01@users.noreply.github.com>
2025-10-13 01:26:35 +0530

    db_connection

--- db_connection/app/views.py
+++ db_connection/app/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+#from django.db. function import Length
 # here we can insert the data into database
 from app.models import*
 
@@ -8,7 +8,9 @@ def insert_topic(request):
     tn=input('enter topic name')
     Tto=Topic.objects.get_or_create(topic_name=tn)
     if Tto[1]:
-        return HttpResponse('Topic name is inserted')
+         QLTO=Topic.objects.all()
+         d={'QLTO': QLTO }
+         return render(request,'display_topic.html' ,d)
     else:
         return HttpResponse('Topic is already present')
     
@@ -23,7 +25,10 @@ def insert_webpage(request):
         TWo=WebPage.objects.get_or_create(topic_name=To,name=name,url=url)
 
         if TWo[1]:
-            return HttpResponse('one row is created in webpage')
+            QLTO=Topic.objects.all()
+            d={'QLTO': QLTO }
+            return render(request,'display_topic.html' ,d)
+
         else:
             return HttpResponse('Given input is already present')
     else:
@@ -31,14 +36,16 @@ def insert_webpage(request):
     
 def insert_accesssrecord(request):
     Wid=int(input('webpageid'))
-    LWo=WebPage.objects.get(id=Wid)
+    LWo=WebPage.objects.filter(id=Wid)
     if LWo:
         Wo=LWo[0]
         author=input('enter name of author')
         date=input('enter date in this fromat yyyy-mm-dd')
         TARO=AccessRecord.objects.get_or_create(name=Wo,author=author,date=date)
         if TARO[1]:
-            return HttpResponse('one row is created in accessrecord')
+            QLARO=AccessRecord.objects.all()
+            d={'QLARO': QLARO }
+            return render(request,'display_accessrecord.html' ,d)
         else:
             return HttpResponse('Given input already present')
     else:
@@ -47,8 +54,35 @@ def insert_accesssrecord(request):
 #here we can display the data from database and send to frontend
 def display_webpage(request):
     QLWO=WebPage.objects.all()
+    #QLWO=WebPage.objects.all()[::-1]
+    QLWO=WebPage.objects.all().order_by('name')
+    #QLWO=WebPage.objects.all().order_by('-name')
+    # QLWO=WebPage.objects.all()[:2:] #here we use slicing to find or retrieve data any where on table
+    # #QLWO=WebPage.objects.all().order_by(length('name'))
+    # #QLWO=WebPage.objects.all().order_by(length('name').desc())# it will arrange the data in descending order in the dat present in name table
+    # QLWO=WebPage.objects.all()
+
+    # QLWO=WebPage.objects.filter(id_in=(2,5))
+
+    QLWO=WebPage.objects.filter(id__range=(2,5))
+
+    # QLWO=WebPage.objects.filter(idgte=2)
+
+    # QLWO=WebPage.objects.filter(name__startswith='R')
+
+    # QLWO=WebPage.objects.filter(name__endswith='T')
+
+    # QLWO=WebPage.objects.filter(name__contains='R')
+
+    # QLWO=WebPage.objects.filter(name__regex='^r\w*')
+
+    # QLWO=WebPage.objects.filter(name__isnull=False)
+
+
+
     d={'QLWO': QLWO }
     return render(request,'display_webpage.html' ,d)
+    
 
 def display_topic(request):
     QLTO=Topic.objects.all()
@@ -57,6 +91,11 @@ def display_topic(request):
 
 def display_accessrecord(request):
     QLARO=AccessRecord.objects.all()
+    
+    #QLAO=AccessRecord.objects.filter(date='2025-10-11')
+    QLARO=AccessRecord. objects. filter(date__month='10')
+    QLARO=AccessRecord.objects.filter(date__year=2025)
+    QLARO=AccessRecord. objects. filter(date__day='11')
     d={'QLARO': QLARO }
     return render(request,'display_accessrecord.html' ,d)
     
