1d5beefbb71ce39816153e73fb3f3e5aca99e061
Rashmi Kanta Biswal <167099176+Rashmikanta01@users.noreply.github.com>
2025-10-15 17:38:40 +0530

    sql_tables

--- sql_tables/app/views.py
+++ sql_tables/app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from app.models import *
+
+def emptodeptjoin(request):
+    QLEDO=EMP.objects.all().select_related('DEPTNO')
+    d={'QLEDO': QLEDO }
+    return render(request,'emptodeptjoin.html' ,d)
